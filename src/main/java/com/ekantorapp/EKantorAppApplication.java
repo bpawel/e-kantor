6636a7182c19c471dcad0f293dd5caf75bd63bd1
p.baluk <pawel.marcin.baluk@gmail.com>
2019-06-26 01:58:36 +0200

    first commit

---
+++ src/main/java/com/ekantorapp/EKantorAppApplication.java
@@ -0,0 +1,13 @@
+package com.ekantorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKantorAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EKantorAppApplication.class, args);
+  }
+
+}
